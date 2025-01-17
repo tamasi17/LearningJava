5a16ad0424fbc6accbef438ba4e073b838866739
tamasi17 <mat.eidelman@gmail.com>
2025-01-17 13:46:54 +0100

    class Fecha sets and equals not working

--- segundaEva/src/ejercicioFecha/Fecha.java
+++ segundaEva/src/ejercicioFecha/Fecha.java
@@ -1,5 +1,7 @@
 package ejercicioFecha;
 
+import java.util.Objects;
+
 public class Fecha {
 
 	
@@ -10,8 +12,157 @@ public class Fecha {
 	
 //	Constructores: uno vacio, otro con los tres parametros.
 	
+	public Fecha() {
+		
+		this.year=0;
+		this.month=1;
+		this.day=1;
+		
+	}
 	
+	public Fecha(int day, int month, int year) {
+		
+		this.year=year;
+		this.month=month;
+		this.day=day;
+	
+		
+	}
 	
 //	Metodos
+	
+	
+	public static boolean fechaCorrecta(int year, int month, int day) {
+		
+		if (month == 4 || month == 6 || month == 9 || month == 11) { //meses con 30 dias
+        	
+        	if (day < 0) {
+        		
+        		return false;
+        	} else if (day <= 30){
+        		
+        		return true;  
+        		
+        	} else {
+        		return false;
+        	}
+        	
+        	
+        	} else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) { //meses con 31 dias
+        		
+            	if (day < 0) {
+            		
+            		return false;
+            		
+            	} else if (day <= 31){
+            		
+            		return true;
+            		
+            	} else {
+            		return false;
+            	}
+        	
+        	} else if (month == 2) { // febrero
+        		
+        		if ((year % 4 == 0) && (year % 100 != 0)) { // bisiesto
+        			
+        			if (day < 0) {
+                		
+        				return false;
+        				
+            	    } else if (day <= 29){
+            		
+            	    	return true;
+            		
+            	    } else {
+
+            	    	return false;            	    
+            	    }
+        			
+        		} else if (day < 0) { // no bisiesto
+        		
+        			return false;
+        			
+        	    } else if (day <= 28){
+        		
+        	    	return true;
+        		
+        	    } else {
+        	    	return false;
+        	    }
+        
+        	} else { // cualquier otro fallo
+        		return false;
+        	}
+    
+		
+	} // fin fechaCorrecta()
+	
+	
+//	Getters y Setters
+
+	public int getYear() {
+		return year;
+	}
+	
+	public void setYear(int year) {
+		
+		if (fechaCorrecta(year, this.month, this.day)) {			
+			this.year = year;
+		}
+	}
+
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+
+		if (fechaCorrecta(this.year, month, this.day)) {			
+			this.month = month;
+		}
+		
+		
+	}
+
+
+	public int getDay() {
+		return day;
+	}
+	
+	public void setDay(int day) {
+
+		if (fechaCorrecta(this.year, this.month, day)) {			
+			this.day = day;
+		}
+	}
+	
+//	toString (generado desde Source)
+	
+	@Override
+	public String toString() {
+		return "Fecha [day=" + day + ", month=" + month + ", year=" + year + "]";
+	}
+
+	
+
+	
+//	equals (generado desde Source)
+	
+	
+	public boolean equals(Fecha f1) {
+		if ( (this.day == f1.day) && (this.month == f1.month) && (this.year == f1.year) ) {
+			return true;
+		} else {
+			return false;
+		}		
+		
+	}
+	
+	
+	
+	
+	
+	
 	
 }
--- segundaEva/src/ejercicioFecha/TestFecha.java
+++ segundaEva/src/ejercicioFecha/TestFecha.java
@@ -4,6 +4,41 @@ public class TestFecha {
 
 	public static void main(String[] args) {
 
+//		Crear instancias de Fecha
+		
+		Fecha fecha1 = new Fecha(1994,12,17);
+		Fecha fecha2 = new Fecha(1997,9,17);
+		
+//		Usar gets, sets, toString y equals.
+		
+		fecha2.setMonth(12);
+		System.out.println(fecha2.toString());
+		
+		System.out.println("");
+		
+		if (fecha1.equals(fecha2)) {
+			System.out.println("Las fechas son iguales");
+		} else {
+			System.out.println("Las fechas son diferentes");
+			
+		}
+		
+		System.out.println("");
+		
+		fecha2.setYear(1994);
+		System.out.println(fecha2.toString());
+		
+		if (fecha1.equals(fecha2)) {
+			System.out.println("Las fechas son iguales");
+		} else {
+			System.out.println("Las fechas son diferentes");
+			
+		}
+		
+		
+		
+		System.out.println(Fecha.fechaCorrecta(1997,9,35));
+		
 		
 		
 	}
