58914158b6ef91549fcaa818dc75c145360d301d
tamasi17 <mat.eidelman@gmail.com>
2025-03-20 16:35:06 +0100

    Added Connect, Insert and Retrieve events to GraficaMongo

--- mongo-java-demo/src/main/java/com/mati/mongodb/mongo_java_demo/GraficaMongo.java
+++ mongo-java-demo/src/main/java/com/mati/mongodb/mongo_java_demo/GraficaMongo.java
@@ -12,6 +12,11 @@ import java.awt.Choice;
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Label;
+import javax.swing.JPanel;
 
 public class GraficaMongo {
 
@@ -49,7 +54,7 @@ public class GraficaMongo {
 		frame = new JFrame();
 		frame.getContentPane().setForeground(new Color(54, 54, 54));
 		frame.getContentPane().setBackground(new Color(54, 54, 54));
-		frame.setBounds(100, 100, 639, 361);
+		frame.setBounds(100, 100, 604, 401);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
@@ -57,21 +62,28 @@ public class GraficaMongo {
 		start.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				// ESCRIBES CODIGO POR AQUI
+				try {
+					MongoConnection.getDatabase("testDAM"); // Define exceptions to throw here
+					nombre.setEnabled(true);
+					rol.setEnabled(true);
+				} catch (Exception e2) {
+					System.out.println("Try again."); 
+				}
+				
 				
 			}
 		});
-		start.setForeground(new Color(0, 128, 64));
-		start.setBackground(new Color(0, 255, 64));
+		start.setForeground(new Color(0, 255, 64));
+		start.setBackground(new Color(128, 128, 128));
 		start.setFont(new Font("Verdana", Font.PLAIN, 11));
-		start.setBounds(220, 32, 183, 34);
+		start.setBounds(203, 32, 183, 34);
 		frame.getContentPane().add(start);
 		
 		nombre = new JTextField();
 		nombre.setText("Name");
 		nombre.setEnabled(false);
 		nombre.setFont(new Font("Verdana", Font.PLAIN, 12));
-		nombre.setBounds(45, 119, 167, 20);
+		nombre.setBounds(45, 119, 190, 20);
 		frame.getContentPane().add(nombre);
 		nombre.setColumns(10);
 		
@@ -80,21 +92,51 @@ public class GraficaMongo {
 		rol.setFont(new Font("Verdana", Font.PLAIN, 12));
 		rol.setEnabled(false);
 		rol.setColumns(10);
-		rol.setBounds(45, 150, 167, 20);
+		rol.setBounds(45, 150, 190, 20);
 		frame.getContentPane().add(rol);
 		
 		JButton insert = new JButton("Insert");
-		insert.setForeground(new Color(0, 128, 64));
+		insert.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				if (nombre.getText()!="Name" && rol.getText()!="Role") {
+					
+					MongoDataInserter.insertData(nombre.getText(), rol.getText());
+
+				}
+				
+			}
+		});
+		insert.setForeground(new Color(0, 255, 64));
 		insert.setFont(new Font("Verdana", Font.PLAIN, 11));
-		insert.setBackground(new Color(0, 255, 64));
-		insert.setBounds(45, 191, 67, 34);
+		insert.setBackground(new Color(73, 73, 73));
+		insert.setBounds(45, 191, 90, 34);
 		frame.getContentPane().add(insert);
 		
+		
+		JTextArea screen = new JTextArea();
+		screen.setWrapStyleWord(true);
+		screen.setForeground(new Color(255, 255, 255));
+		screen.setLineWrap(true);
+		screen.setText("The data from your document will appear here.");
+		screen.setBackground(new Color(73, 73, 73));
+		screen.setBounds(282, 118, 264, 109);
+		frame.getContentPane().add(screen);
+		
 		JButton retrieve = new JButton("Retrieve");
-		retrieve.setForeground(new Color(0, 128, 64));
+		retrieve.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String result = MongoDataRetriever.retrieveData("name", nombre.getText());
+				screen.setText(result);
+				System.out.println("\n Resultado: \n " + result);
+			}
+		});
+		retrieve.setForeground(new Color(0, 255, 64));
 		retrieve.setFont(new Font("Verdana", Font.PLAIN, 11));
-		retrieve.setBackground(new Color(0, 255, 64));
-		retrieve.setBounds(122, 191, 90, 34);
+		retrieve.setBackground(new Color(73, 73, 73));
+		retrieve.setBounds(145, 191, 90, 34);
 		frame.getContentPane().add(retrieve);
+		
 	}
 }
