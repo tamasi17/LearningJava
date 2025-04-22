677fdbce0482dd8287fcc31d7ca0e2b9ff960939
tamasi17 <mat.eidelman@gmail.com>
2025-04-22 13:44:55 +0200

    started pruebaColecciones

---
+++ terceraEva/src/pruebaColecciones/TestColecciones.java
@@ -0,0 +1,19 @@
+package pruebaColecciones;
+
+public class TestColecciones {
+
+	public static void main(String[] args) {
+
+		
+		String pedro = "Pedro";
+		String paco = "Paco";
+		String pamela = "Pamela";
+		String pablo = "Pablo";
+		String penelope = "Penelope";
+		
+		
+		
+		
+	}
+
+}
